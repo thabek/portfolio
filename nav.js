099d757eb8246238ebb869ac205328ac018f0968
Artur Gniazdowski <a.gniazdowski@artgeist.com>
2025-05-25 11:29:11 +0200

    hamburger

--- nav.js
+++ nav.js
@@ -1,17 +1,15 @@
 function openNav() {
 	document.getElementById("nav").style.width = "75%";
+	document.getElementById("nav").style.boxShadow = "8px 0 10px 5px black";
 	document.getElementById("nav-ul").style.opacity = "100%";
-	document.getElementById("hamburger-close").style.opacity = "100%";
-	document.getElementById("hamburger-close").style.display = "block";
-	document.getElementById("hamburger").style.opacity = "0%";
+	document.getElementById("hamburger-close").style.display = "initial";
 	document.getElementById("hamburger").style.display = "none";
 
 }
 function closeNav() {
 	document.getElementById("nav").style.width = "0%";
+	document.getElementById("nav").style.boxShadow = "none";
 	document.getElementById("nav-ul").style.opacity = "0%";
-	document.getElementById("hamburger-close").style.opacity = "0%";
 	document.getElementById("hamburger-close").style.display = "none";
-	document.getElementById("hamburger").style.opacity = "100%";
-	document.getElementById("hamburger").style.display = "block";
+	document.getElementById("hamburger").style.display = "initial";
 }
